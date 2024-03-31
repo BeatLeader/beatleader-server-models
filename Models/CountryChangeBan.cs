232a01bf2dab8fec8382fc2aad9f612c7c73153c
NSGolova <golova@golova.dev>
2024-03-31 23:07:58 +0100

    username pfp change ban

--- Models/CountryChangeBan.cs
+++ Models/CountryChangeBan.cs
@@ -6,4 +6,11 @@
         public string PlayerId { get; set; }
         public int Timeset { get; set; }
     }
+
+    public class UsernamePfpChangeBan
+    {
+        public int Id { get; set; }
+        public string PlayerId { get; set; }
+        public int Timeset { get; set; }
+    }
 }
