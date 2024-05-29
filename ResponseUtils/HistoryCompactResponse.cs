8c59ae7398f459dde02b6393035b30342723db9a
NSGolova <golova@golova.dev>
2024-05-29 11:34:18 +0100

    Added compact history response

---
+++ ResponseUtils/HistoryCompactResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatLeader_Server.Utils {
+    public class HistoryCompactResponse {
+        public int Timestamp { get; set; }
+
+        public float Pp { get; set; }
+        public int Rank { get; set; }
+        public int CountryRank { get; set; }
+
+        public float AverageRankedAccuracy { get; set; }
+        public float AverageUnrankedAccuracy { get; set; }
+        public float AverageAccuracy { get; set; }
+
+        public float MedianRankedAccuracy { get; set; }
+        public float MedianAccuracy { get; set; }
+
+        public int RankedPlayCount { get; set; }
+        public int UnrankedPlayCount { get; set; }
+        public int TotalPlayCount { get; set; }
+    }
+}
