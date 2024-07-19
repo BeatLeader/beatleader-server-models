7f2e7bca3aadc23c77593e10281ea71d5133c24c
NSGolova <golova@golova.dev>
2024-07-19 04:58:48 +0100

    Week100 polls

---
+++ Models/Week100Poll.cs
@@ -0,0 +1,9 @@
+﻿namespace BeatLeader_Server.Models {
+    public class Week100Poll {
+        public int Id { get; set; }
+        public string PlayerId { get; set; }
+        public string Key { get; set; }
+
+        public bool Filled { get; set; }
+    }
+}
