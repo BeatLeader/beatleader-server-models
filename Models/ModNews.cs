305fb90f0e7f47a93d22475e87d6bdc9e118a369
NSGolova <golova@golova.dev>
2024-08-03 21:48:36 +0100

    Added mod news

---
+++ Models/ModNews.cs
@@ -0,0 +1,11 @@
+﻿namespace BeatLeader_Server.Models {
+    public class ModNews {
+        public int Id { get; set; }
+        public string Owner { get; set; }
+        public string OwnerIcon { get; set; }
+
+        public int Timepost { get; set; }
+        public string Body { get; set; }
+        public string Image { get; set; }
+    }
+}
