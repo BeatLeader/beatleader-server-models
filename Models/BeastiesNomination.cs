cc28d560cf4150d810ed3164fc30d571928e9b87
NSGolova <golova@golova.dev>
2024-08-09 01:58:43 +0100

    Beasties nominations

---
+++ Models/BeastiesNomination.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeatLeader_Server.Models {
+    public class BeastiesNomination {
+        public int Id { get; set; }
+
+        [StringLength(25, MinimumLength = 0)]
+        public string PlayerId { get; set; }
+        [StringLength(25, MinimumLength = 0)]
+        public string LeaderboardId { get; set; }
+        [StringLength(50, MinimumLength = 0)]
+        public string Category { get; set; }
+        public int Timepost { get; set; }
+    }
+}
