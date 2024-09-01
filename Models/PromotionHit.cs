32e37cddbb37a2f2c606e58480483223d0a37eeb
NSGolova <golova@golova.dev>
2024-09-01 14:11:56 +0100

    Added promotion dowmnloads recording

---
+++ Models/PromotionHit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeatLeader_Server.Models {
+    public class PromotionHit {
+        public int Id { get; set; }
+        public int Promotion { get; set; }
+        public int Timeset { get; set; }
+        [StringLength(25, MinimumLength = 0)]
+        public string UniqueId { get; set; }
+        [StringLength(50, MinimumLength = 0)]
+        public string Details { get; set; }
+    }
+}
