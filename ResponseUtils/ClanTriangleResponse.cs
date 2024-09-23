930c005b7f52dc4702accd05ab5f4af80385086c
NSGolova <golova@golova.dev>
2024-09-23 22:44:11 +0100

    Added clan triangles

---
+++ ResponseUtils/ClanTriangleResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace BeatLeader_Server.Utils {
+    public class ClanTrianglePlayer {
+        public string Id { get; set; }
+        public string Avatar { get; set; }
+
+        public float AccPp { get; set; }
+        public float TechPp { get; set; }
+        public float PassPp { get; set; }
+    }
+
+    public class ClanTriangleResponse {
+        public float AccPp { get; set; }
+        public float TechPp { get; set; }
+        public float PassPp { get; set; }
+
+        public ICollection<ClanTrianglePlayer> Players { get; set; }
+    }
+}
