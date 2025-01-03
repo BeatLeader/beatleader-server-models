dc40fd03c0956b6ef1457ba9614f41029f939d02
NSGolova <golova@golova.dev>
2025-01-03 19:15:25 +0000

    Leaderboard scores acc graph

---
+++ ResponseUtils/LeaderboardStatsRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatLeader_Server.Utils {
+
+    public class LeaderboardStatsNote {
+        public float SpawnTime { get; set; }
+        public int Score { get; set; }
+        public float Accuracy { get; set; }
+    }
+
+    public class LeaderboardStatsGraph {
+        public List<LeaderboardStatsNote> Notes { get; set; }
+        public int Rank { get; set; }
+        public string PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerAvatar { get; set; }
+        public string Modifiers { get; set; }
+    }
+
+    public class LeaderboardStatsRecord {
+        public List<LeaderboardStatsGraph> Scores { get; set; }
+    }
+}
