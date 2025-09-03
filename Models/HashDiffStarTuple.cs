9116476e4157ace5c2c901fe94212ba400604533
NSGolova <golova@golova.dev>
2025-09-03 20:50:23 +0000

    Removed class that uses protobuf

--- Models/HashDiffStarTuple.cs
+++
@@ -1,6 +0,0 @@
-﻿using ProtoBuf;
-
-namespace Models.Models;
-
-[ProtoContract(SkipConstructor = true)]
-public record struct HashDiffStarTuple([property: ProtoMember(1)] string Hash, [property: ProtoMember(2)] string Diff, [property: ProtoMember(3)] float Stars);
